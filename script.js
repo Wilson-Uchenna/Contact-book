815b6bb185079891c57f5d6c9cb06e7c7d9eb8ee
Wilson-Uchenna <wilsonuchenna08@gmail.com>
2023-05-28 18:01:58 +0100

    functional

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const errorMessage = document.getElementById("demo1")
 const inputField = document.getElementById("inputfield")
 function validContact() {
     let inputValue = inputField.value;
-    // const str1 = inputValue.split(",");
-      //for (let i = 0; i < inputValue.length; i++) {
-        if (!isNaN(inputValue)) {
+           // const str1 = inputValue.split(",");
+        // for (let i = 0; i < str1.length; i++) {
+        if ((/^[A-Z]/).test(inputValue) && (/inputValue(?=[a-z])/)) {
+            errorMessage.textContent = "";
+        } //else if() {
+            //errorMessage.textContent = "Please input Desired Value";
+    /*}*/ else  if (/inputValue(?=(\d))/) {
             errorMessage.textContent = "Please input Desired Value";
-        } else if((/[a-z](?=[^a-z])/).test(inputValue)) {
-            errorMessage.textContent = "Please input Desired Value";
-        } else {
-            errorMessage.textContent ="";
-        }
-    //}
+         }
+     return errorMessage
 }
