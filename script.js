88dee1d947fef4ba720d36197e19e371a6f9e174
Wilson-Uchenna <wilsonuchenna08@gmail.com>
2023-06-18 13:18:00 +0100

    changed

--- script.js
+++ script.js
@@ -1,15 +1,51 @@
-const errorMessage = document.getElementById("demo1")
-const inputField = document.getElementById("inputfield")
+const errorMessage = document.getElementById("demo1");
+const inputField = document.getElementById("inputfield");
+
 function validContact() {
     let inputValue = inputField.value;
-           // const str1 = inputValue.split(",");
-        // for (let i = 0; i < str1.length; i++) {
-        if ((/^[A-Z]/).test(inputValue) && (/inputValue(?=[a-z])/)) {
-            errorMessage.textContent = "";
-        } //else if() {
-            //errorMessage.textContent = "Please input Desired Value";
-    /*}*/ else  if (/inputValue(?=(\d))/) {
-            errorMessage.textContent = "Please input Desired Value";
-         }
-     return errorMessage
+  const regex = /^[A-Z]/;
+
+  if (!regex.test(inputValue)) {
+    errorMessage.textContent = "Please input a value starting with an uppercase letter.";
+    return;
+  }
+
+  if (/\s+/g.test(inputValue)) {
+    inputValue = inputValue.replace(/\s+/g, "");
+  }
+
+  if (/[^a-zA-Z]/.test(inputValue)) {
+    errorMessage.textContent = "Please input a value without numbers.";
+    return;
+  }
+
+  errorMessage.textContent = "";
+  return inputValue.trim();
 }
+
+const inputPhone = document.getElementById("number")
+const errorMessage2 = document.getElementById("input")
+function validPhone() {
+    let phoneValue = inputPhone.value;
+    if (/\s+/g.test(phoneValue)) {
+      phoneValue = phoneValue.replace(/\s+/g, "");
+    }
+  
+    if (/[^0-9]/.test(phoneValue)) {
+      errorMessage2.textContent = "Please input a value with numbers.";
+      return;
+    }
+  
+    errorMessage2   .textContent = "";
+    return phoneValue.trim();
+  }
+
+
+  const demo = document.getElementById("demo")
+
+  function writePhone() {
+    const inputValue = validPhone();
+    if (inputValue !== undefined) {
+        demo.textContent = inputValue;  
+    }
+  } 
