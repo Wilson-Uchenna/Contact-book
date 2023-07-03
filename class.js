affb4835ae80774faaf6340005d282df1bf7632c
Wilson-Uchenna <wilsonuchenna08@gmail.com>
2023-07-03 14:17:54 +0100

    added functionality

--- class.js
+++ class.js
@@ -21,10 +21,18 @@ document.addEventListener('DOMContentLoaded', function() {
     targetElement.insertAdjacentElement('afterbegin', newElement);
                 })
  })
-
+const itemID = document.getElementsByClassName('second-section-container');
     function searchContact() {
         const contactValue = searchContactName.value;
-        for (let i = 0; i <name.length;i++) {
-        const filteredNames = names.filter((name) => name[i].includes(contactValue))
-        }
+        for (let i = 0; i < itemID.length; i++) {
+            const name = itemID[i].querySelector('.second-section-item .section-section-item p');
+            nameElement = name.textContent;
+
+            if(nameElement.includes(contactValue)) {
+                itemID[i].style.display = 'block';
+            }
+            else {
+                itemID[i].style.display = 'none';
+            }
+    }
     }
